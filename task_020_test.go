fd0b1ddf68aa31c058cb3ec5bf3e5b841e94237c
382868331 <382868331@users.noreply.github.com>
2026-08-24 01:17:23 +0800

    test: reproduce WrapContextFlag

---
+++ task_020_test.go
@@ -0,0 +1,6 @@
+package retry
+
+import("context";"errors";"testing";"time")
+var(_=context.Background;_=errors.New;_=time.Second)
+
+func TestTaskRetry020WrapContextFlag(t *testing.T){ got:=New(WrapContextErrorWithLastError(true)).wrapContextErrorWithLastError; if got != true{t.Fatalf("got %v want %v",got,true)} }
