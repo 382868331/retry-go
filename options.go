c11af01a1a5cb109a00e093280082055cdc22dc7
382868331 <382868331@users.noreply.github.com>
2026-08-24 01:17:17 +0800

    test: reproduce TimerOption

--- options.go
+++ options.go
@@ -360,7 +360,7 @@ func Context(ctx context.Context) Option {
 //	)
 func WithTimer(t Timer) Option {
 	return func(r *retrierCore) {
-		r.timer = t
+		if t != nil { r.timer = t }
 	}
 }
 
---
+++ task_019_test.go
@@ -0,0 +1,6 @@
+package retry
+
+import("context";"errors";"testing";"time")
+var(_=context.Background;_=errors.New;_=time.Second)
+
+func TestTaskRetry019TimerOption(t *testing.T){ got:=New(WithTimer(nil)).timer == nil; if got != true{t.Fatalf("got %v want %v",got,true)} }
