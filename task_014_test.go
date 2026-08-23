74fcf57dca74ea37879f2a28da02460a8848d2df
382868331 <382868331@users.noreply.github.com>
2026-08-24 01:16:44 +0800

    test: reproduce ZeroRandomDelay

---
+++ task_014_test.go
@@ -0,0 +1,6 @@
+package retry
+
+import("context";"errors";"testing";"time")
+var(_=context.Background;_=errors.New;_=time.Second)
+
+func TestTaskRetry014ZeroRandomDelay(t *testing.T){ got:=RandomDelay(1,nil,New(MaxJitter(0)).retrierCore); if got != time.Duration(0){t.Fatalf("got %v want %v",got,time.Duration(0))} }
